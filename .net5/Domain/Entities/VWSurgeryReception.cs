bac4e16f76a2c4d080abb074ca543295e3d4165f
MinaBahaghighat <61118112+vestaabner@users.noreply.github.com>
2022-10-19 13:49:30 +0330

    feat fix folderas

---
+++ .net5/Domain/Entities/VWSurgeryReception.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiladHosSignalR.Domain.Entities
+{
+    [Table("VWSurgeryReception", Schema = "srg")]
+    public class VWSurgeryReception
+    {
+        public int LastPositionId { get; set; }
+        public string LastPositionName { get; set; }
+        public int Id { get; set; }
+        public int SurgeryReceptionId { get; set; }
+        public int SurgicalStatusId { get; set; }
+        public char SurgeryDate { get; set; }
+        public string PatientName { get; set; }
+        public string PatientFirstName { get; set; }
+        public string PatientFatherName { get; set; }
+    }
+
+}
+
---
+++ .net5/Infrastructures/HosHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using MiladHosSignalR.Domain.Entities;
+
+namespace MiladHosSignalR.Infrastructures
+{
+    public class HosHub : Hub
+    {
+    }
+
+
+    public static class ShipmentsHubExtensions
+    {
+        public static async Task newChanges<TMessage>(this IHubContext<HosHub> hubContext,
+            List<VWSurgeryReception> data,
+            CancellationToken cancellationToken = default)
+        {
+            await hubContext.Clients
+                .All
+                .SendAsync("newChanges",
+                    data,
+                    cancellationToken);
+        }
+    }
+}
