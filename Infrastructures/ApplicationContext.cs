ec0e6cef6eef0ef663cdc8edb02969c507a2d6d6
vestaabner <61118112+vestaabner@users.noreply.github.com>
2022-10-19 12:04:33 +0330

    feat change dotnet versions

---
+++ Infrastructures/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MiladHosSignalR.Domain.Entities;
+
+namespace MiladHosSignalR.Infrastructures
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<VWSurgeryReception> VWSurgeryReception { get; set; }
+      
+    }
+}
